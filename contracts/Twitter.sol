e9a87ebe99c94299be69e4ad783f841564578c7c
AndreQuintero <andre.quintero96@gmail.com>
2024-12-28 01:51:24 +0800

    create, get, and validation of owner and limit of characters implementation

---
+++ contracts/Twitter.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Twitter {
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+    }
+    uint16 public TWEEET_MAX_LENGTH = 280;
+    mapping (address => Tweet[]) public tweets;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You're not allowed to execute this operation");
+        _;
+    }
+
+    function changeTweetLength(uint16 newTweetLength) public onlyOwner {
+        TWEEET_MAX_LENGTH = newTweetLength;
+    }
+
+    function createTweet(string memory _tweet) public {
+        require(bytes(_tweet).length <= TWEEET_MAX_LENGTH, "Your tweet exceeded the limit of characters");
+
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        return tweets[msg.sender].push(newTweet);
+    }
+
+    function getTweet(address _owner, uint256 i) public view returns (Tweet memory) {
+        return tweets[_owner][i];
+    }
+
+    function getALLTweets(address _owner) public view returns (Tweet[] memory) {
+        return tweets[_owner];
+    }
+
+}
